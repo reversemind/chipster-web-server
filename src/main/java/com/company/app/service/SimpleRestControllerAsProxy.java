19c57a9ae889a5ad7e0e696c51fd844633272ca5
Eugene Kalinin <kalinin.eugene@gmail.com>
2017-06-20 15:14:27 +0300

    Proxy Http/WebSocket - for example

---
+++ src/main/java/com/company/app/service/SimpleRestControllerAsProxy.java
@@ -0,0 +1,7 @@
+package com.company.app.service;
+
+/**
+ *
+ */
+public class SimpleRestControllerAsProxy {
+}
--- src/main/java/fi/csc/chipster/proxy/model/Connection.java
+++ src/main/java/fi/csc/chipster/proxy/model/Connection.java
@@ -10,55 +10,66 @@ import org.apache.logging.log4j.Logger;
 
 @XmlRootElement // json
 public class Connection {
-	
-	@SuppressWarnings("unused")
-	// TODO replace on lombok
+
+    @SuppressWarnings("unused")
+    // TODO replace on lombok
 //	private static final Logger logger = LogManager.getLogger();
-	
-	private Route route;
-	private String sourceAddress;
-	private String requestURI;
-	private LocalDateTime openTime;
-	private LocalDateTime closeTime;
-	private String method;
-	
-	public String getSourceAddress() {
-		return sourceAddress;
-	}
-	public void setSourceAddress(String sourceAddress) {
-		this.sourceAddress = sourceAddress;
-	}
-	public LocalDateTime getOpenTime() {
-		return openTime;
-	}
-	public void setOpenTime(LocalDateTime openTime) {
-		this.openTime = openTime;
-	}
-	public LocalDateTime getCloseTime() {
-		return closeTime;
-	}
-	public void setCloseTime(LocalDateTime closeTime) {
-		this.closeTime = closeTime;
-	}
-	public String getRequestURI() {
-		return requestURI;
-	}
-	public void setRequestURI(String requestPath) {
-		// remove query string to hide tokens
-		UriBuilder builder = UriBuilder.fromUri(requestPath);
-		builder.replaceQuery(null);
-		this.requestURI = builder.toString();
-	}
-	public Route getRoute() {
-		return route;
-	}
-	public void setRoute(Route route) {
-		this.route = route;
-	}
-	public String getMethod() {
-		return method;
-	}
-	public void setMethod(String method) {
-		this.method = method;
-	}
+
+    private Route route;
+    private String sourceAddress;
+    private String requestURI;
+    private LocalDateTime openTime;
+    private LocalDateTime closeTime;
+    private String method;
+
+    public String getSourceAddress() {
+        return sourceAddress;
+    }
+
+    public void setSourceAddress(String sourceAddress) {
+        this.sourceAddress = sourceAddress;
+    }
+
+    public LocalDateTime getOpenTime() {
+        return openTime;
+    }
+
+    public void setOpenTime(LocalDateTime openTime) {
+        this.openTime = openTime;
+    }
+
+    public LocalDateTime getCloseTime() {
+        return closeTime;
+    }
+
+    public void setCloseTime(LocalDateTime closeTime) {
+        this.closeTime = closeTime;
+    }
+
+    public String getRequestURI() {
+        return requestURI;
+    }
+
+    public void setRequestURI(String requestPath) {
+        // remove query string to hide tokens
+        UriBuilder builder = UriBuilder.fromUri(requestPath);
+        builder.replaceQuery(null);
+        this.requestURI = builder.toString();
+    }
+
+    public Route getRoute() {
+        return route;
+    }
+
+    public void setRoute(Route route) {
+        this.route = route;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
 }
